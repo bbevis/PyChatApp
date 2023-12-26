d925c5a04b82b8d6bb7b542f48137c4ac73e2c22
bbevis <burintbevis@hotmail.com>
2023-12-26 12:09:20 +0000

    updated style

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from flask_socketio import SocketIO, join_room, leave_room, send
+import random
+import string
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "supersecretkey"
@@ -8,10 +10,9 @@ socketio = SocketIO(app)
 # A mock database to persist data
 rooms = {}
 
-# TODO: Build the routes
 def generate_room_code(length: int, existing_codes: list[str]) -> str:
     while True:
-        code_chars = [random.choice(ascii_letters) for _ in range(length)]
+        code_chars = [random.choice(string.ascii_letters) for _ in range(length)]
         code = ''.join(code_chars)
         if code not in existing_codes:
             return code
@@ -46,8 +47,57 @@ def home():
         return redirect(url_for('room'))
     else:
         return render_template('home.html')
+    
+@app.route('/room')
+def room():
+    room = session.get('room')
+    name = session.get('name')
+    if name is None or room is None or room not in rooms:
+        return redirect(url_for('home'))
+    messages = rooms[room]['messages']
+    return render_template('room.html', room=room, user=name, messages=messages)
 
-# TODO: Build the SocketIO event handlers
+@socketio.on('connect')
+def handle_connect():
+    name = session.get('name')
+    room = session.get('room')
+    if name is None or room is None:
+        return
+    if room not in rooms:
+        leave_room(room)
+    join_room(room)
+    send({
+        "sender": "",
+        "message": f"{name} has entered the chat"
+    }, to=room)
+    rooms[room]["members"] += 1
+    
+@socketio.on('message')
+def handle_message(payload):
+    room = session.get('room')
+    name = session.get('name')
+    if room not in rooms:
+        return
+    message = {
+        "sender": name,
+        "message": payload["message"]
+    }
+    send(message, to=room)
+    rooms[room]["messages"].append(message)
+    
+@socketio.on('disconnect')
+def handle_disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+        send({
+        "message": f"{name} has left the chat",
+        "sender": ""
+    }, to=room)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
